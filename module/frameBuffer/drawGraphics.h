ef37eca0d022652d2863485d95c743fb9243bbd5
vm-cc <13568859409@163.com>
2019-03-31 00:50:54 -0700

    Delete unuse interface function

--- module/frameBuffer/drawGraphics.h
+++ module/frameBuffer/drawGraphics.h
@@ -21,22 +21,4 @@ int LcdDrawPoint(unsigned int x,unsigned int y,unsigned int color);
  */
 void LcdDrawLine(unsigned int x1,unsigned int y1,unsigned int x2,unsigned int y2,unsigned int color);
 
-/**
- *@brief LCD显示字符串
- *@param x1 y1：起点坐标 x2 y2:终点坐标 color:字符颜色
- */
-void LcdDrawStr(unsigned int x1,unsigned int y1,unsigned int x2,unsigned int y2,unsigned int color,char *ptr);
-
-/**
- *@brief LCD画按钮
- *@param x1 y1：起点坐标 width height:按钮的长和宽
- *       str:按钮中的字符 str-color:字符颜色 back-color:背景颜色
- */
-void LcdDrawButton(unsigned int x1,unsigned int y1,unsigned int width,unsigned int height,
-                   char *str,unsigned int str_color,unsigned int back_color);
-
-
-
-
-
 
